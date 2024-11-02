4fb554ee7c1a28eafd1d14e230cc53084cc1fa68
aerwk <eykli@proton.me>
2024-11-02 13:53:43 +0800

    update test.py with tutorial code

--- test.py
+++ test.py
@@ -1,7 +1,24 @@
-print("Hello motherfuckers.")
-print("Please type motherfucker below to confirm you are one of us.")
-client_input = input(" ").lower()
-if(client_input == "motherfucker"):
-    print("Welcome to motherfucker land.")
-else:
-    print("You're a fatherfucker.")
+import datetime
+def meow():
+    print("Hello motherfuckers.")
+    print("Please type motherfucker below to confirm you are one of us.")
+    client_input = input(" ").lower()
+    if(client_input == "motherfucker"):
+        print("Welcome to motherfucker land.")
+    else:
+        print("You're a fatherfucker.")
+
+# meow()
+
+object_example = {
+    "name": "Eric",
+    "age": 100
+}
+
+print(object_example["age"])
+
+list = [1, 2, 2]
+
+print(list.count(2))
+
+print(datetime.datetime.now().isoweekday())
