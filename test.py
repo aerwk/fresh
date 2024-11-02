ffb5d12722abd24f132f37df6a95501a01712be0
aerwk <eykli@proton.me>
2024-11-02 12:50:41 +0800

    initialise project

---
+++ test.py
@@ -0,0 +1,7 @@
+print("Hello motherfuckers.")
+print("Please type motherfucker below to confirm you are one of us.")
+client_input = input(" ").lower()
+if(client_input == "motherfucker"):
+    print("Welcome to motherfucker land.")
+else:
+    print("You're a fatherfucker.")
